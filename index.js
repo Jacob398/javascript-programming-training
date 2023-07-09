f0a761c294a3999d4225bf239ab6f878594e8ef7
Jacob398 <Jacob14MB@gmail.com>
2023-07-09 14:28:11 -0500

    day 2 of training

--- index.js
+++ index.js
@@ -1,5 +1,86 @@
 //explain why's and how's in comments
 //this is my first javascript code!
-console.log('Hello World');
-let name = 'Jacob';
-console.log(name);
+
+//console.log('Hello World');
+//let name = 'Jacob';
+//console.log(name);
+
+//variable rules
+// cannot be a reserved keyword
+//should be meaningful
+//cannot start with a number (1name)
+//cannot contain a space or hyphen(-)
+//are case-sensitive
+// dynamic and static languages are different. Dynamic languages like javascript, a type of a variable can change at runtime.
+
+let firstName = 'Jacob '; //string literal
+let lastName = 'Brown';
+
+//let age = 20; //number literal
+let isApproved = true; //boolean literal
+let hello; //undefined literal
+let hello1 = undefined;
+let goodbye = null;
+//we us null in situations where we want to clear the value of a variable 
+let selectedColors = ['red', 'blue'];
+console.log(selectedColors);
+selectedColors[2] = 'green';
+console.log(selectedColors[0]);
+console.log(selectedColors);
+//in javascript we can store different types in an array
+selectedColors[3] = 22;
+console.log(selectedColors);
+//Here you can see that the array has added 22 into the list of items it contains
+console.log(selectedColors.length);
+// this property returns the number of items or elements in the array
+
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+console.log(firstName);
+console.log(typeof firstName)
+//let name = firstName + lastName
+let person = {
+    name: 'Jacob',
+    age: 30
+};
+
+
+
+//Dot Notation
+person.name = 'John';
+//Dot notation is cleaner and easier to use.
+
+//Bracket notation
+person['name'] = 'Mary';
+
+
+//console.log(name)
+//console.log(person);
+console.log(person.name)
+//performing a task
+function greet(name, lastName) {
+   console.log('Hello ' + name + ' ' + lastName);
+}
+
+//function that calculates a value
+function square(number){
+    return number * number
+}
+
+let number = square(2);
+console.log(number)
+
+greet('John', 'Smith');
+greet('Mary', 'Jane');
+
+console.log(typeof interestRate)
+
+//learn the difference between parameter and argument
+//A parameter is the input at the time of declaration, and the argument is the actual value with supply for parameter
+//So in this case, the parameter would be "name" in 'function greet(name)' and the argument would be John
+//Seperate arguments using a comma ","
+
+
+
+
